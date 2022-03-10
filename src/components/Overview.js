60efb6caa99411a3f8ab3d4bff2171785258a636
Avery Burne <burneav@gmail.com>
2022-03-10 15:47:03 -0500

    Finished Overview component and added Repos component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -3,9 +3,29 @@ import { GithubContext } from '../context/GithubContext'
 
 const Overview = () => {
     const { overview } = useContext(GithubContext)
+    let id = 0
 
         return (
-            <h1>Overview sheesh</h1>
+            <div className='overviewContainer'>
+                {overview && overview.map((item, index) => (
+                    <a
+                    key={index}
+                    href={item.html_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                        <div classNamed={'overview overview-' + ++id}>
+                            {item.name && <h2>{item.name}</h2>}
+                            {item.language && (
+                                <p>
+                                    <span></span>
+                                    {item.language}
+                                </p>
+                            )}
+                        </div>
+                    </a>
+                ))}
+            </div>
         )
 }
 
---
+++ src/components/Repos.js
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react'
+import { GithubContext } from '../context/GithubContext'
+
+const Repos = () => {
+
+    return (
+
+    )
+}
+
+export default Repos
